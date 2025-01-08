c28d8dfa35f69ee388e3dfbc9b0e61de579c2e8a
GAFT <trieunguyen04239@gmail.com>
2025-01-08 14:40:20 +0700

    sprinkling

--- main.tf
+++ main.tf
@@ -1,37 +1,44 @@
-provider "aws" {
-  region = "eu-north-1"
+provider "google" {
+  credentials = file("~/.ssh/key.json")
+  project = "video-streaming-446003"
+  region  = "us-central1"
 }
 
-resource "aws_key_pair" "ansible_key" {
-  key_name   = "ansible-key"
-  public_key = file("~/.ssh/roadmap.pub")  
-}
+resource "google_compute_instance" "ansible_ready_instance" {
+  name         = "ansible-instance"
+  machine_type = "e2-small"
+  zone         = "europe-north1-a"
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  boot_disk {
+    initialize_params {
+      image = "projects/ubuntu-os-cloud/global/images/ubuntu-2404-noble-amd64-v20241219"
+    }
   }
-   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"  # Allow all outbound traffic
-    cidr_blocks = ["0.0.0.0/0"]  # Allow to any destination
+
+  network_interface {
+    network       = "default"
+    access_config {} # Ephemeral external IP
   }
+
+  metadata = {
+    ssh-keys = "your-username:$(cat ~/.ssh/id_ed25519.pub)"
+  }
+
+  tags = ["allow-ssh"]
 }
 
-resource "aws_instance" "ansible_ready_instance" {
-  ami             = "ami-08eb150f611ca277f"  # Ubuntu t3 24.04 AMI ID
-  instance_type   = "t3.micro"
-  key_name        = aws_key_pair.ansible_key.key_name
-  security_groups = [aws_security_group.allow_ssh.name]
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = "default"
 
-  tags = { Name = "Roadmap-project" }
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
 }
 
 output "instance_ip" {
-  value = aws_instance.ansible_ready_instance.public_ip
+  value = google_compute_instance.ansible_ready_instance.network_interface[0].access_config[0].nat_ip
 }
